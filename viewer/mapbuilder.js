9e223f6595984f5701910248809ece2d07ea65bc
Antonio Dolcetta <antonio.dolcetta@gmail.com>
2013-10-25 19:31:31 +0800

    small tweaks

--- viewer/mapbuilder.js
+++ viewer/mapbuilder.js
@@ -2,54 +2,60 @@ var svg = d3.select("#board").append("svg")
 var circle = svg.selectAll("circle");
 
 $(document).ready(function(){
-draw_game(game);
-
+  draw_game(game);
 })
 
 function berlingame2d3format(berlin){
-	var nodes=[]
+  //berlin better not lie about node ids or have any missing
+  //let's hope it's consistent about 1 indexing them too
+  
+  var nodes = {}
   var links=[]
-	//berlin better not lie about node ids or have any missing.
-	
-  $.each(berlin.map.nodes, function(index,guy){
-		nodes[guy.id-1]={"name":guy.id-1,"group":1, "type": guy.type}
-   })
-  $.each(berlin.state, function(index,guy){
-		nodes[guy.node_id-1].units = guy.number_of_soldiers
-		nodes[guy.node_id-1].owner = guy.player_id
-   })
+  
+  for (var key in berlin.map.nodes) {
+    var bnode = berlin.map.nodes[key]
+    nodes[bnode.id-1]={
+      "name": bnode.id-1, "type": bnode.type }
+  }
 
-   if (nodes.length!=berlin.map.nodes.length){
-		debugger;
-	  rwiefjfew;
-	 }
-   $.each(berlin.map.paths, function(index,guy){
-		links.push({'source':(guy.from-1),'target':(guy.to-1), 'value':1})
-	 })	 
-	var res={'nodes':nodes,'links':links}
-	return res
+  for (var index in berlin.state ) {
+    state = berlin.state[index]
+    nodes[state.node_id-1].units = state.number_of_soldiers
+    nodes[state.node_id-1].owner = state.player_id
+  }
+
+  for (var key in berlin.map.paths) {
+    path = berlin.map.paths[key]
+    links.push({'source':(path.from-1), 'target':(path.to-1), 'value':1})
+  } 
+
+  var nodelist=[]
+  for (var key in nodes) {
+    nodelist.push(nodes[key])
+  }
+  
+  var res={'nodes':nodelist,'links':links}
+  return res
 }
 
 function draw_game(berlin){
-	d3game=berlingame2d3format(game);
-	var width = 1100,
-    height = 800;
+  
+  var width = 1100;
+  var height = 800;
+  d3game=berlingame2d3format(game, width, height);
+  var color = d3.scale.category20();
 
-var color = d3.scale.category20();
-
-var force = d3.layout.force()
-    .charge(-100)
-    .linkDistance(30)
-    .size([width, height]);
-
-var svg = d3.select("#board").append("svg")
+  var svg = d3.select("#board").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-  force
-      .nodes(d3game.nodes)
-      .links(d3game.links)
-      .start();
+  var force = d3.layout.force()
+    .charge(-800)
+    .linkDistance(60)
+    .linkStrength(1.0)
+    .friction(0.95) // higher values = better untangling
+    .gravity(0.05)
+    .size([width, height]);
 
   var link = svg.selectAll(".link")
       .data(d3game.links)
@@ -61,22 +67,24 @@ var svg = d3.select("#board").append("svg")
       .data(d3game.nodes)
     .enter().append("circle")
       .attr("class", function(d) { return "node-"+d.type })
-      .attr("r", function(d) { return d.type == "city" ? 8 : 2 })
+      .attr("r", function(d) { return d.type == "city" ? 30 : 20 })
       .style("fill", function(d) { return color(d.owner); })
-
-			.call(force.drag);
+      .call(force.drag);
+  
+  force.nodes(d3game.nodes)
+    .links(d3game.links)
+    .start();
 
   node.append("title")
-      .text(function(d) { return d.name; });
+    .text(function(d) { return d.name; });
 
   force.on("tick", function() {
     link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
     node.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
+      .attr("cy", function(d) { return d.y; });
   });
-	return force
+  return force
 };
